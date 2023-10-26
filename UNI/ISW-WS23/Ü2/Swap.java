9df0e519daac9be5fabf7ab530a48500f030f598
Gianni M. Gagliardi PC <bf284@uni-heidelberg.de>
2023-10-26 20:08:12 +0200

    added Ü2

---
+++ UNI/ISW-WS23/Ü2/Swap.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class Swap
+{
+    private String id;
+    private Trainer trainer1;
+    private Trainer trainer2;
+    private Pokemon pokemon1;
+    private Pokemon pokemon2;
+    private Date date;
+
+    public Swap(String id, Trainer trainer1, Trainer trainer2, Pokemon pokemon1, Pokemon pokemon2)
+    {
+        this.id = id;
+        this.trainer1 = trainer1;
+        this.pokemon1 = pokemon1;
+        this.pokemon2 = pokemon2;
+        this.date = new Date(0, 0, 0, 0, 0, 0);
+    }
+
+    public void execute()
+    {
+        //if()
+
+
+        //if()
+
+
+        //trainer1.ge
+    }
+}
